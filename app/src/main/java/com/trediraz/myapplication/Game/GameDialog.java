12a2e18c1907d3c17ef31446d4e4843e03791514
Michał Szczepanik <trediraz@gmail.com>
2019-08-10 18:58:52 +0200

    Improve new game dialog views

--- app/src/main/java/com/trediraz/myapplication/Game/GameDialog.java
+++ app/src/main/java/com/trediraz/myapplication/Game/GameDialog.java
@@ -2,13 +2,20 @@ package com.trediraz.myapplication.Game;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
@@ -18,9 +25,13 @@ import androidx.fragment.app.DialogFragment;
 
 import com.trediraz.myapplication.R;
 
+import java.util.Objects;
+
 public class GameDialog extends DialogFragment {
 
-    private ViewFlipper flipper;
+    private ViewFlipper mViewFlipper;
+    private boolean requiresScenario = false;
+    private String gameName;
 
     @NonNull
     @Override
@@ -34,14 +45,42 @@ public class GameDialog extends DialogFragment {
     @Override
     public void onStart() {
         super.onStart();
-        flipper = getDialog().findViewById(R.id.create_game_flipper);
+        mViewFlipper = getDialog().findViewById(R.id.create_game_flipper);
         Button nextButton = getDialog().findViewById(R.id.next_button);
         Button previousButton = getDialog().findViewById(R.id.previous_button);
+        Button addExpansionButton = getDialog().findViewById(R.id.add_expansion_button);
+
+        CheckBox requiresScenarioCheckBox = getDialog().findViewById(R.id.requires_scenario_checkbox);
+        requiresScenarioCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                requiresScenario = b;
+            }
+        });
+
+        Spinner spinner = getDialog().findViewById(R.id.game_type_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(Objects.requireNonNull(getContext()),R.array.game_types,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                handleNextAction();
+                int currentView =  mViewFlipper.getDisplayedChild();
+                boolean isDataValid = true;
+                switch (currentView){
+                    case 0:
+                        EditText gameNameText = getDialog().findViewById(R.id.game_name);
+                        gameName = gameNameText.getText().toString();
+                        if (gameName.equals("")){
+                            isDataValid = false;
+                            Toast.makeText(getContext(), R.string.no_name_toast, Toast.LENGTH_SHORT).show();
+                        }break;
+                    case 1:
+                        Toast.makeText(getContext(),"OPTIONAL",Toast.LENGTH_SHORT).show();
+                }
+                if(isDataValid)
+                    handleNextAction();
             }
         });
         previousButton.setOnClickListener(new View.OnClickListener() {
@@ -65,24 +104,62 @@ public class GameDialog extends DialogFragment {
                     return false;
             }
         });
-    }
 
-    private void handleNextAction() {
-        View lastView = getDialog().findViewById(R.id.number_of_players);
-        if(flipper.getCurrentView() != lastView){
-            flipper.showNext();
-        }
-        else
-            dismiss();
+        addExpansionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout linearLayout = getDialog().findViewById(R.id.expansions);
+                EditText lastEditText = (EditText) linearLayout.getChildAt(linearLayout.getChildCount()-1);
+                if(!lastEditText.getText().toString().equals("")) {
+                    EditText editText = new EditText(getContext());
+                    editText.setHint(R.string.expansion_name);
+                    editText.setTextAlignment(EditText.TEXT_ALIGNMENT_CENTER);
+                    linearLayout.addView(editText,linearLayout.getChildCount());
+                    editText.requestFocus();
+                }
+            }
+        });
     }
 
     private void handlePreviousAction(){
-        View firstView = getDialog().findViewById(R.id.name);
-        if(flipper.getCurrentView() != firstView) {
-            Log.d("BGr","eql");
-            flipper.showPrevious();
-        }
-        else
+
+        int currentView = mViewFlipper.getDisplayedChild();
+        boolean skipOptionalView = requiresScenario;
+        final int OPTIONAL_VIEW = 1;
+
+        if (currentView == 0) {
             dismiss();
+        } else if(currentView == OPTIONAL_VIEW + 1 && skipOptionalView){
+            mViewFlipper.setDisplayedChild(OPTIONAL_VIEW - 1);
+        }
+        else{
+            mViewFlipper.showPrevious();
+        }
+    }
+
+    private void handleNextAction() {
+        hideKeyboard();
+        int currentView = mViewFlipper.getDisplayedChild();
+        boolean skipOptionalView = requiresScenario;
+        final int OPTIONAL_VIEW = 1;
+
+        if (currentView == mViewFlipper.getChildCount() - 1) {
+            dismiss();
+        } else if(currentView == (OPTIONAL_VIEW - 1) && skipOptionalView){
+            mViewFlipper.setDisplayedChild(OPTIONAL_VIEW + 1);
+        } else{
+            mViewFlipper.showNext();
+        }
+    }
+
+    private void hideKeyboard() {
+        View view = getDialog().getCurrentFocus();
+        if(view != null){
+            view.clearFocus();
+            InputMethodManager inputMethodManager = (InputMethodManager) Objects.requireNonNull(getContext()).getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (inputMethodManager != null) {
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
+            }
+        }
     }
 }
