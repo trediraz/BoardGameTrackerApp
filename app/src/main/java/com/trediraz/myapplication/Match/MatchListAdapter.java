34d0952e1cacc55f4d617175e76da29d66c0ca8a
Michał Szczepanik <trediraz@gmail.com>
2019-08-20 17:11:14 +0200

    Change match adapter layout

--- app/src/main/java/com/trediraz/myapplication/Match/MatchListAdapter.java
+++ app/src/main/java/com/trediraz/myapplication/Match/MatchListAdapter.java
@@ -2,12 +2,10 @@ package com.trediraz.myapplication.Match;
 
 import android.app.Activity;
 import android.content.Context;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.trediraz.myapplication.Database.Match;
