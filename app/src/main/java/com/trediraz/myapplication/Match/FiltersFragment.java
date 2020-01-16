01eeedd7612db4fe85c90fdf945de834f1e8729d
Michał Szczepanik <trediraz@gmail.com>
2020-01-16 17:21:43 +0100

    Add scenarioSpinner to filter fragment

--- app/src/main/java/com/trediraz/myapplication/Match/FiltersFragment.java
+++ app/src/main/java/com/trediraz/myapplication/Match/FiltersFragment.java
@@ -7,13 +7,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.trediraz.myapplication.Database.Scenario;
 import com.trediraz.myapplication.MainActivity;
 import com.trediraz.myapplication.R;
 
@@ -47,6 +50,14 @@ public class FiltersFragment extends Fragment {
         gameAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         gameSpinner.setAdapter(gameAdapter);
 
+        Spinner scenarioSpinner = getView().findViewById(R.id.scenario_spinner);
+        List<String> scenarioNames = new ArrayList<>();
+        scenarioNames.add("-");
+        ArrayAdapter<String> scenarioAdapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,scenarioNames);
+        scenarioAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        scenarioSpinner.setAdapter(scenarioAdapter);
+        scenarioSpinner.setEnabled(false);
+
         gameSpinner.setSelection(gameNames.indexOf(Objects.requireNonNull(filters).gameName));
 
         Button saveButton = getView().findViewById(R.id.save_button);
@@ -55,6 +66,38 @@ public class FiltersFragment extends Fragment {
             FiltersFragmentDirections.ToMatches action = FiltersFragmentDirections.toMatches();
             action.setFilters(filters);
             Navigation.findNavController(getView()).navigate(action);
+        });
+
+        gameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                scenarioNames.clear();
+                String gameName = (String) parent.getItemAtPosition(position);
+                if(gameName.equals(getString(R.string.all))){
+                    scenarioNames.add("-");
+                }
+                else{
+                    List<Scenario> scenarios = MainActivity.mBoardGameDao.getScenariosByGameName(gameName);
+                    if(scenarios.size() > 1){
+                        scenarioNames.add(getString(R.string.all));
+                    }
+                    for(Scenario s : scenarios){
+                        if(s.name.equals(Scenario.DEFAULT_NAME))
+                            scenarioNames.add("Podstawka");
+                        else
+                            scenarioNames.add(s.name);
+                    }
+                }
+                scenarioSpinner.setEnabled(scenarioNames.size() > 1);
+                scenarioSpinner.setAdapter(null);
+                scenarioSpinner.setAdapter(scenarioAdapter);
+                scenarioSpinner.setSelection(0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
 
         Button clearButton = Objects.requireNonNull(getView()).findViewById(R.id.clear_button);
